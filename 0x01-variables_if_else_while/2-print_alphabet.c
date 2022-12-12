d945d700475bb247f6caa597edfeda4f42f59ccd
Abiodun-Onajobi Treasure <90309222+treasureTheWhiz@users.noreply.github.com>
2022-12-12 01:37:49 -0800

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,43 +1,22 @@
-nclude <stdio.h>
-
-
+#include <stdio.h>
 
 /**
-
  * main - main prints in lower and upper case
-
  *
-
  * Return: return 0
-
  */
 
-
-
 int main(void)
-
 {
-
 	char alpha;
 
-
-
 	for (alpha = 'a'; alpha <= 'z'; alpha++)
-
 	{
-
 		putchar(alpha)
-
 		if (alpha == 'z')
-
 		{
-
 			putchar('\n');
-
 		}
-
 	}
-
 	return (0);
-
 }
