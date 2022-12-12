3c59ad898583f7377ba104232bd09b0c6d53371b
treasureTheWhiz <ifethayo2020@gmail.com>
2022-12-12 01:35:08 -0800

    hello

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,43 @@
+nclude <stdio.h>
+
+
+
+/**
+
+ * main - main prints in lower and upper case
+
+ *
+
+ * Return: return 0
+
+ */
+
+
+
+int main(void)
+
+{
+
+	char alpha;
+
+
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+
+	{
+
+		putchar(alpha)
+
+		if (alpha == 'z')
+
+		{
+
+			putchar('\n');
+
+		}
+
+	}
+
+	return (0);
+
+}
