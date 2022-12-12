deb10efa981d4c603203e96df908163533ee7009
Abiodun-Onajobi Treasure <90309222+treasureTheWhiz@users.noreply.github.com>
2022-12-12 01:46:26 -0800

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,2 +1,20 @@
-hello
+#include <stdio.h>
+#include <stdlib.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int alphabets;
+
+	for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
+		putchar(alphabets);
+	for (alphabets = 'A'; alphabets <= 'Z'; alphabets++)
+		putchar(alphabets);
+	putchar('\n');
+	return (0);
+}
